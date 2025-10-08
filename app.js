11226f45bcea46c915084617efaadcef7c4184a2
Kharel Pawan <kharelpawan.np@gmail.com>
2025-10-08 09:45:56 +0545

    cors issue resolved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 //const helmet = require('helmet');
-//const cors = require('cors');
+const cors = require('cors');
 //const morgan = require('morgan');
 //const path = require('path');
 
@@ -20,7 +20,8 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 //app.use(helmet());
-//app.use(cors());
+app.use(cors());
+app.options(/.*/, cors());
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 //app.use(morgan('dev'));
