b512074ec4c283113368c92b930ce74ba2b3423b
Kharel Pawan <kharelpawan.np@gmail.com>
2025-10-09 19:46:18 +0545

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const {getUsers, getUser, createUser, updateUser, deleteUser} = require("../controllers/userController");
+const { protect, authorize } = require('../middleware/auth');
+//yo way ma global vayo 
+// router.use(protect);
+// router.use(authorize('superadmin'));
+router.get(
+  '/',
+  protect,
+  authorize('superadmin'),
+  getUsers
+);
+router.post(
+  '/',
+  protect,
+  authorize('superadmin'),
+  createUser
+);
 
-router.route('/').get(getUsers).post(createUser);
+//router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
